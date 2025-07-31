b83a3d6cc65ed0a5c627242a8d49276f87bf45c6
samuel-udoh <techchuks16@gmail.com>
2025-07-31 20:09:51 +0100

    0x5

--- alx-project-0x05/context/CountContext.tsx
+++ alx-project-0x05/context/CountContext.tsx
@@ -1,12 +1,12 @@
 import { createContext, useContext, useState, ReactNode } from "react";
 
-interface CountContextProp {
+interface CountContextProps {
     count: number;
     increment: () => void
     decrement: () => void
 }
 
-export const CountContext = createContext<CountContextProp | undefined>(undefined)
+export const CountContext = createContext<CountContextProps | undefined>(undefined)
 
 export const CountProvider = ({children}: {children: ReactNode}) => {
     const [count, setCount] = useState<number>(0)
